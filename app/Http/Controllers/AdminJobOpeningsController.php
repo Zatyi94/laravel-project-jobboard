0262d93df519136fcf038fdc8cef20ae6a2e3e84
Zatyi94 <zatyko.nora@gmail.com>
2022-05-24 15:25:17 +0200

    Add create

--- app/Http/Controllers/AdminJobOpeningsController.php
+++ app/Http/Controllers/AdminJobOpeningsController.php
@@ -15,4 +15,28 @@ class AdminJobOpeningsController extends Controller
 
         return view('admin.job_openings.index')->with("viewData", $viewData);
     }
+
+    public function create()
+    {
+        $viewData = [];
+        $viewData["title"] = "Admin Page - Job Openings - Create New";
+
+        return view('admin.job_openings.create')->with("viewData", $viewData);
+    }
+
+    public function store(Request $request)
+    {
+        // var_dump('Form: ', $request->input('country'));
+
+        $request->validate([
+            "country" => "required|max:255",
+            "job_description" => "required",
+        ]);
+
+        $jobOpening = new JobOpening();
+        $jobOpening->setCountry($request->input('country'));
+        // $jobOpening->save();
+
+        return redirect()->route('admin.job_openings.index');
+    }
 }
