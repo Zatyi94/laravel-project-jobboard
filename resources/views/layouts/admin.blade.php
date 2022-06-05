4cd7ba44961dfa5e3fcd666f0eabfce859c18b43
Zatykó Nóra <zatyko.nora@gmail.com>
2022-06-05 14:27:18 +0200

    change the admin profile image

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -34,7 +34,7 @@
         <div class="col content-grey">
             <nav class="p-3 shadow text-end">
                 <span class="profile-font">Admin</span>
-                <img class="img-profile rounded-circle" src="{{ asset('/img/icon-user.png') }}">
+                <img class="img-profile rounded-circle" src="{{ asset('/img/undraw_profile.svg') }}">
             </nav>
             <div class="g-0 m-5">
                 @yield('content')
